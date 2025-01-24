650d146747618d5f4ea2eb1063f8c5cc562d8449
Sahara Sipple <sahara36@usf.edu>
2025-01-24 00:28:23 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 1: Variables //
+
+let employeeName = "Lucy"; // String
+const employeeID = 1234; // Number
+var isActive = true; // Boolean
+console.log(typeof employeeName);  // String
+console.log(typeof employeeID);  // Number
+console.log(typeof isActive);  // Boolean
