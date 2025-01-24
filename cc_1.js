ca10a5bb5c8a34060a839a33bb01adbf4c855c86
Sahara Sipple <sahara36@usf.edu>
2025-01-24 00:37:31 -0500

    Update to Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -3,15 +3,15 @@
 let employeeName = "Lucy"; // String
 const employeeID = 1234; // Number
 var isActive = true; // Boolean
-console.log(typeof employeeName);  // String
-console.log(typeof employeeID);  // Number
-console.log(typeof isActive);  // Boolean
+console.log(employeeName, typeof employeeName);  // String
+console.log(employeeID, typeof employeeID);  // Number
+console.log(isActive, typeof isActive);  // Boolean
 
 // Task 2: Primitive Data Types //
 
 let productName = "Coffee" // String
 const productPrice = 4 // Number
 var isAvailable = true; // Boolean
-console.log(typeof productName); //String
-console.log(typeof productPrice); //Number
-console.log(typeof isAvailable); //Boolean
+console.log(productName, typeof productName); //String
+console.log(productPrice, typeof productPrice); //Number
+console.log(isAvailable, typeof isAvailable); //Boolean
