b38ff3731a3df6e76522b1c12bcc6d6c9bf675f5
Sahara Sipple <sahara36@usf.edu>
2025-01-24 00:33:46 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -1,8 +1,17 @@
 // Task 1: Variables //
 
 let employeeName = "Lucy"; // String
-const employeeID = 1234; // Number
-var isActive = true; // Boolean
 console.log(typeof employeeName);  // String
+const employeeID = 1234; // Number
 console.log(typeof employeeID);  // Number
+var isActive = true; // Boolean
 console.log(typeof isActive);  // Boolean
+
+// Task 2: Primitive Data Types //
+
+let productName = "Coffee" // String
+console.log(typeof productName); //String
+const productPrice = 4 // Number
+console.log(typeof productPrice); //Number
+var isAvailable = true; // Boolean
+console.log(typeof isAvailable); //Boolean
